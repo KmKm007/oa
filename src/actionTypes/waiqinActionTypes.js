0b915d582020c031be8cd88cb51476291073fd6c
hxm <dgdkhxm@163.com>
2017-03-27 16:09:56 +0800

    外勤相关actionTypes

---
+++ src/actionTypes/waiqinActionTypes.js
@@ -0,0 +1,15 @@
+export const SIGN_IN = 'SIGN_IN'
+
+export const REQUEST_LOCATION = 'GET_LOCATION'
+
+export const FETCH_LOCATION = 'FETCH_LOCATION'
+
+export const RECEIVE_LOCATION = 'RECEIVE_LOCATION'
+
+export const REQUEST_ADDRESS = 'REQUEST_ADDRESS'
+
+export const FETCH_ADDRESS = 'FETCH_ADDRESS'
+
+export const RECEIVE_ADDRESS = 'RECEIVE_ADDRESS'
+
+export const SHOW_ADDRESS = 'SHOW_ADDRESS'
