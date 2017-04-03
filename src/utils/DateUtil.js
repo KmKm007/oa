357afe64d53dc3b81b3519a554f37efd0c2596d7
hxm <dgdkhxm@163.com>
2017-04-03 11:03:33 +0800

    时间工具类

---
+++ src/utils/DateUtil.js
@@ -0,0 +1,19 @@
+export function parseToTimeObject(time) {
+  const date = new Date(time)
+  const year = date.getFullYear()
+  const month = date.getMonth()
+  const day = date.getDate()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSecnds()
+  const millisecond = date.getMilliseconds()
+  return {
+    year,
+    month: month >= 9 ? (month + 1) : '0' + (month + 1),
+    day: day >= 10 ? day : ('0' + day),
+    hour: hour >= 10 ? hour : ('0' + hour),
+    minute: minute >= 10 ? minute : ('0' + minute),
+    second: second >= 10 ? second : ('0' + second),
+    millisecond
+  }
+}
