982d6a0636ec6ea5cbf2d613b36b8f384de31436
hxm <dgdkhxm@163.com>
2017-04-06 09:03:43 +0800

    更新react-router到4.0

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 import React from 'react'
-import { Router, Route, hashHistory as history, Redirect } from 'react-router'
+import { HashRouter as Router, Route, Redirect} from 'react-router-dom'
 import SignPage from '../page/waiqin/SignPage'
 import SignSucceedPage from '../page/waiqin/SignSucceedPage'
 import RemarkPage from '../page/waiqin/RemarkPage'
 
 const AppRouter = () => (
   <Router history={history}>
-      <Route path="/waiqin/sign" component={SignPage}/>
-      <Route path="/waiqin/signSucceed" component={SignSucceedPage}/>
-      <Route path="/waiqin/remark" component={RemarkPage} />
-      <Redirect from="/" to="/waiqin/sign" />
+      <div>
+        <Route path="/waiqin/sign" component={SignPage}/>
+        <Route path="/waiqin/signSucceed" component={SignSucceedPage}/>
+        <Route path="/waiqin/remark" component={RemarkPage} />
+        <Redirect from="/" to="/waiqin/sign" />
+      </div>
   </Router>
 )
 
