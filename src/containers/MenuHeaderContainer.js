a0808f860efe9bf1e79468a94464c5c8ad0ac057
hxm <dgdkhxm@163.com>
2017-04-06 09:02:03 +0800

    添加返回，注册事件功能

--- src/containers/MenuHeaderContainer.js
+++ src/containers/MenuHeaderContainer.js
@@ -1,14 +1,54 @@
 import React from 'react'
 import cs from 'classnames'
+import createHistory from 'history/createHashHistory'
 
-const MenuHeaderContainer = ({ leftLabel, rightLabel, title }) => {
-  return (
-    <div className="header">
-      <div className={cs('text-center', 'headerLeftContainter')}>{leftLabel || '返回'}</div>
-      <h2 className={cs('text-center', 'headerMiddleContainer')}>{title}</h2>
-      <div className={cs('text-center', 'headerRightContainer')}>{ rightLabel || ''}</div>
-    </div>
-  )
+const history = createHistory()
+
+class MenuHeaderContainer extends React.Component {
+  static defaultProps = {
+    defaultLeftLabel: '返回',
+    handleDefaultLeftClick: () => {
+      history.goBack()
+    }
+  }
+
+  render () {
+    const {
+      leftLabel,
+      rightLabel,
+      title,
+      handleLeftClick,
+      handleRightClick,
+      handleDefaultLeftClick,
+      defaultLeftLabel
+    } = this.props
+    const onLeftClick = handleLeftClick ? handleLeftClick : handleDefaultLeftClick
+    const onRightClick = handleRightClick
+    return (
+      <div className="header">
+        <div className={cs('text-center', 'headerLeftContainter')}>
+          <a onClick={e => {
+            e.preventDefault()
+            onLeftClick()
+          }}>
+            {leftLabel || defaultLeftLabel}
+          </a>
+        </div>
+        <h2 className={cs('text-center', 'headerMiddleContainer')}>{title}</h2>
+        <div className={cs('text-center', 'headerRightContainer')}>
+          <a onClick={e => {
+            e.preventDefault()
+            if (typeof(onRightClick) === 'function')
+              onRightClick()
+          }}>
+              { rightLabel || ''}
+          </a>
+        </div>
+      </div>
+    )
+  }
 }
+
+
 
 export default MenuHeaderContainer
