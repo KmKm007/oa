f1c6ae37ac07b8acdee3e52e276f21fa544878e1
hxm <dgdkhxm@163.com>
2017-04-06 15:25:16 +0800

    更新

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,17 @@ import React from 'react'
 import { HashRouter as Router, Route, Redirect} from 'react-router-dom'
 import SignPage from '../page/waiqin/SignPage'
 import SignSucceedPage from '../page/waiqin/SignSucceedPage'
+import ListChildUserPage from '../page/waiqin/ListChildUserPage'
 import RemarkPage from '../page/waiqin/RemarkPage'
 
 const AppRouter = () => (
-  <Router history={history}>
+  <Router>
       <div>
         <Route path="/waiqin/sign" component={SignPage}/>
         <Route path="/waiqin/signSucceed" component={SignSucceedPage}/>
         <Route path="/waiqin/remark" component={RemarkPage} />
-        <Redirect from="/" to="/waiqin/sign" />
+        <Route path="/waiqin/childUsers" component={ListChildUserPage}/>
+        <Redirect from="/" to="/waiqin/sign"/>
       </div>
   </Router>
 )
