e7801092b428f25f6f7be6137ab9d727403507f4
hxm <dgdkhxm@163.com>
2017-04-08 00:17:21 +0800

    新增加载效果

--- src/page/waiqin/ListChildUserPage.jsx
+++ src/page/waiqin/ListChildUserPage.jsx
@@ -1,7 +1,8 @@
-import React, { PropTypes } from 'react'
-import { connect } from 'react-redux'
+import React, {PropTypes} from 'react'
+import {connect} from 'react-redux'
 import MenuHeaderContainer from '../../containers/MenuHeaderContainer'
 import actions from '../../actions'
+import actionTypes from '../../actionTypes'
 import createHistory from 'history/createHashHistory'
 import Loading from '../../components/Loading'
 
@@ -19,35 +20,49 @@ class ListChildUserPage extends React.Component {
   componentWillMount() {
     document.title = this.props.title
     const userDetail = this.props.userDetail
-    let userId = userDetail ? userDetail.userId : 5151
+    let userId = userDetail.userId
     this.props.fetchUserChildren(userId)
   }
 
   onUserClick = userId => {
     userId = userId || this.props.userDetail.userId
-    history.push(`/waiqin/history/${userId}`)
+    this.props.handleFetchWaiqinHistory(userId)
+    history.push('/waiqin/history')
   }
 
-  render () {
-    const { userDetail, children } = this.props
-    const childList = children ? (
-      children.map(user => (
-        <li className="child-body">
-          <div className="child-body-left-container">
-            <span className="child-label">{user.name}</span>
-            <span className="child-position">{`(${user.position})`}</span>
-          </div>
-          <div>
-            <button className="show-history-btn" onClick={() => this.onUserClick(user.userId)}>查看</button>
-          </div>
-        </li>
-      ))
-    ) : <Loading />
-
-
+  render() {
+    const {userDetail, children, errorMesg} = this.props
+    if (!userDetail) {
+      return <Loading loadingText="获取个人信息中..."/>
+    }
+    let content
+    if (errorMesg) {
+      content = <div className="errorMesg">查找下属列表失败！错误信息为：{errorMesg}</div>
+    } else {
+      if (children) {
+        content = (
+          <ul>
+            {children.map(user => (
+                <li className="child-body">
+                  <div className="child-body-left-container">
+                    <span className="child-label">{user.name}</span>
+                    <span className="child-position">{`(${user.position})`}</span>
+                  </div>
+                  <div>
+                    <button className="show-history-btn" onClick={() => this.onUserClick(user.userId)}>查看</button>
+                  </div>
+                </li>
+              ))
+            }
+          </ul>
+        )
+      } else {
+        content =  <Loading loadingText="获取下属列表中..."/>
+      }
+    }
     return (
       <div className="container">
-        <MenuHeaderContainer />
+        <MenuHeaderContainer/>
         <div className="myself-container">
           <div className="myself-body-container">
             <div className="myself-body">
@@ -56,26 +71,31 @@ class ListChildUserPage extends React.Component {
                 <span className="child-position">{`(${userDetail.position})`}</span>
               </div>
               <div>
-                <button className="show-history-btn" onClick={this.onUserClick}>查看</button>
+                <button className="show-history-btn" onClick={() => this.onUserClick(userDetail.userId)}>查看</button>
               </div>
             </div>
           </div>
         </div>
         <div className="child-container">
-          {childList}
+          {content}
         </div>
       </div>
     )
   }
 }
 
-const stateToProps = state => ({
-  userDetail: state.user.detail,
-  children: state.user.children
-})
+const stateToProps = state => {
+  const errorObject = state.user.errorMesgArray.find(mesg => mesg.errorType === actionTypes.RECEIVE_USER_CHILDREN_FAILED)
+  return {
+    userDetail: state.user.detail,
+    children: state.user.children,
+    errorMesg: errorObject ? errorObject.errorMesg : null
+  }
+}
 
 const dispatchToProps = dispatch => ({
-  fetchUserChildren: userId => dispatch(actions.fetchUserChildren(userId))
+  fetchUserChildren: userId => dispatch(actions.fetchUserChildren(userId)),
+  handleFetchWaiqinHistory: userId => dispatch(actions.fetchWaiqinHistory(userId))
 })
 
 export default connect(stateToProps, dispatchToProps)(ListChildUserPage)
